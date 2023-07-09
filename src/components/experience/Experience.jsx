8ce8a6c1ca12123d90afc55b0d3b6770e3b0a969
Mohammed Lashein <ben10gravattack@gmail.com>
2023-07-09 18:45:03 +0300

    finished experience component

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,6 +1,48 @@
+// BsFillPatchCheckFill
+import {BsFillPatchCheckFill} from 'react-icons/bs'
+import frontEndExperience from './front-end-experience'
+import backEndExperience from './back-end-experience'
+import './experience.css'
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+        <h5>What skills I have</h5>
+        <h2>My Experience</h2>
+        <div className="container experience__container">
+          <div className="experience__front-end">
+            <h3>frontend development</h3>
+            <div className="experience__technologies">
+            {frontEndExperience.map((experience) => {
+              return (
+                  <article className="experience__techonology" key={experience.id}>
+                    <experience.icon className='experience__icon'/>
+                    <div>
+                    <div className='title'>{experience.title}</div>
+                    <small className='text-light'>{experience.level}</small>
+                    </div>
+                  </article>
+              )
+            })}
+              </div>
+          </div>
+          <div className="experience__back-end">
+            <h3>Backend development</h3>
+          <div className="experience__technologies">
+            {backEndExperience.map((experience) => {
+              return (
+                <article className="experience__techonology" key={experience.id}>
+                  <experience.icon className='experience__icon'/>
+                  <div>
+                    <div className='title'>{experience.title}</div>
+                    <small className='text-light'>{experience.level}</small>
+                    </div>
+                </article>
+            )
+            })}
+          </div>
+          </div>
+      </div>
+    </section>
   )
 }
 export default Experience
---
+++ src/components/experience/back-end-experience.js
@@ -0,0 +1,32 @@
+import {BsFillPatchCheckFill} from 'react-icons/bs'
+
+const backEndExperience = [
+    {   id : 'Node JS',
+        title: 'Node JS',
+        level: 'Intermediate',
+        icon: BsFillPatchCheckFill,
+    },
+    {   id : 'Mongo DB',
+        title: 'Mongo DB',
+        level: 'Experienced',
+        icon: BsFillPatchCheckFill,
+    },
+    {   id : 'PHP',
+        title: 'PHP',
+        level: 'Basic',
+        icon: BsFillPatchCheckFill,
+    },
+    {   id : 'MySQL',
+        title: 'MySQL',
+        level: 'Intermediate',
+        icon: BsFillPatchCheckFill,
+    },
+    {   id : 'Python',
+        title: 'Python',
+        level: 'Intermediate',
+        icon: BsFillPatchCheckFill,
+    },
+];
+
+
+export default backEndExperience;
