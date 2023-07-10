b91bc77578a14d3702c245793ab783a96a44ffa5
Mohammed Lashein <ben10gravattack@gmail.com>
2023-07-10 11:59:46 +0300

    finished the testimonials component and used swiper js in the component

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,7 +1,80 @@
-import './testimonials.css';
+
+
+
+// import function to register Swiper custom elements
+import { register } from 'swiper/element/bundle';
+// register Swiper custom elements
+register();
+
+import "./testimonials.css";
+import { Pagination } from 'swiper/modules';
+
 const Testimonials = () => {
-  return (
-    <section id='testimonials'>Testimonials</section>
-  )
-}
-export default Testimonials
+  /* Note: swiper-slide uses kebab-case so on tags and attributes , so
+  we should change className to be class so that our styling would
+  have effect .  */
+	return (
+		<section id="testimonials">
+			<h5>Review from clients</h5>
+			<h2>Testimonials</h2>
+
+			<swiper-container class="container testimonials__container" slides-per-view='1' space-between='50' Pagination='true'>
+
+				<swiper-slide class="testimonial">
+					<div className="client__avatar">
+						<img src="https://via.placeholder.com/300" alt="client avatar" />
+					</div>
+					<h5 className="client__name">client name</h5>
+					<small className="client__review">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+						aliquid ipsa earum dignissimos architecto, eius cupiditate, facere
+						quis eum ea deleniti obcaecati sint. Aperiam voluptate ea
+						accusantium laborum consequatur quia.
+					</small>
+				</swiper-slide>
+        
+
+				<swiper-slide class="testimonial">
+					<div className="client__avatar">
+						<img src="https://via.placeholder.com/300" alt="client avatar" />
+					</div>
+					<h5 className="client__name">client name</h5>
+					<small className="client__review">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+						aliquid ipsa earum dignissimos architecto, eius cupiditate, facere
+						quis eum ea deleniti obcaecati sint. Aperiam voluptate ea
+						accusantium laborum consequatur quia.
+					</small>
+				</swiper-slide>
+
+				<swiper-slide class="testimonial">
+					<div className="client__avatar">
+						<img src="https://via.placeholder.com/300" alt="client avatar" />
+					</div>
+					<h5 className="client__name">client name</h5>
+					<small className="client__review">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+						aliquid ipsa earum dignissimos architecto, eius cupiditate, facere
+						quis eum ea deleniti obcaecati sint. Aperiam voluptate ea
+						accusantium laborum consequatur quia.
+					</small>
+				</swiper-slide>
+
+				<swiper-slide class="testimonial">
+					<div className="client__avatar">
+						<img src="https://via.placeholder.com/300" alt="client avatar" />
+					</div>
+					<h5 className="client__name">client name</h5>
+					<small className="client__review">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+						aliquid ipsa earum dignissimos architecto, eius cupiditate, facere
+						quis eum ea deleniti obcaecati sint. Aperiam voluptate ea
+						accusantium laborum consequatur quia.
+					</small>
+				</swiper-slide>
+
+			</swiper-container>
+		</section>
+	);
+};
+export default Testimonials;
